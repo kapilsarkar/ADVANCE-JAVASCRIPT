941b9aa32025fb6a4b7e03f6de11a4d387941933
kapilsarkar <k.sarkar81@gmail.com>
2023-07-05 05:50:07 +0530

    mathobjecttruthyandfalsyvaluesinjavascript

--- JAVA SCRIPT REVISION/PART-4/script.js
+++ JAVA SCRIPT REVISION/PART-4/script.js
@@ -5,3 +5,42 @@ console.log(exp)
 
 //SQUARE ROOT 
 
+console.log(Math.sqrt(25))
+console.log(Math.SQRT2)
+
+//MATH.POW--
+console.log(Math.pow(2,5))
+
+//MATH.FLOOR--
+console.log(Math.floor(2.3))
+console.log(Math.floor(2.99))
+
+//MATH.CEIL---
+
+console.log(Math.ceil(2.3))
+console.log(Math.ceil(2.9))
+
+//MATH.ROUND---
+console.log(Math.round(4.6))
+console.log(Math.round(4.2))
+
+//MATH.RANDOM---
+console.log(Math.random())
+console.log(Math.random()*20)
+console.log(Math.floor(Math.random()*20))
+console.log(Math.ceil(Math.random()*20))
+
+//FIND A NUMBER BETWEEN 10 AND 20 AND REMOVE THE DECIMAL PART---
+
+const num1 = 10;
+const num2 = 20;
+const ans = Math.floor(Math.random()* (num2-num1)+num1)
+console.log(ans)
+
+//TRUTHY AND FALSY VALUES---
+const number1 =  Boolean(10)
+console.log(number1)
+
+const number2 = Boolean(0)
+console.log(number2)
+
