09c7b419a175709fdd726b1b447a68aae354ee99
kapilsarkar <k.sarkar81@gmail.com>
2023-07-07 06:18:46 +0530

    initialcommit

---
+++ JAVA SCRIPT REVISION/PART-7/script.js
@@ -0,0 +1,21 @@
+//Objects in java Script---
+
+//There is only one type of non-primitive data type that is object
+
+const user = {
+    firstName : 'Kapil'
+}
+console.log(user)
+console.log(user.firstName)
+
+const user1 = {
+    fName:'Kapil',
+    lName :'Sarkar'
+}
+
+const user2 = {
+    fName :'Kapil',
+    lName :'Sarkar'
+}
+
+console.log(user1===user2)  //False  because  their address are different in memory they are  also called reference types because there address are different in memory
