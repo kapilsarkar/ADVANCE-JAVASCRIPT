ec61b677a1d149d6b1c870adc1888cf61fd773ca
kapilsarkar <k.sarkar81@gmail.com>
2023-07-13 12:22:03 +0530

    scopesindepthinjavascript

---
+++ JAVA SCRIPT REVISION/PART-13/script.js
@@ -0,0 +1,60 @@
+//Scope in Jva Script---
+const userName = "Kapil";
+let age = 32;
+var a = 50;
+
+function add() {
+  const x = 10;
+  const y = 20;
+  console.log(x + y);
+}
+add();
+
+console.log("Program Ended");
+
+console.log(window.userName);
+console.log(window.age);
+console.log(window.a);
+console.log(window.add);
+
+//Lexical Scope in Java Script---
+
+function subtract() {
+  const x = 60;
+  const y = 10;
+  const z = 30;
+  const w = 100;
+  console.log(x - y);
+  function child() {
+    const childName = "Sona Baba";
+    const m = 200;
+    console.log(childName);
+    console.log(z);
+    function grandChild() {
+      const grandChildName = "Piku";
+      console.log(grandChildName);
+      console.log(m - w);
+    }
+    grandChild();
+  }
+  child();
+}
+subtract();
+
+//Block Scope---
+
+function One() {
+  const first = 100;
+  console.log(first);
+
+  function Two() {
+    const second = 200;
+    console.log(second);
+    {
+      const three = 300;
+      console.log(three);
+    }
+  }
+  Two();
+}
+One();
