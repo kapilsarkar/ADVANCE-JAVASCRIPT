5df78f9441b8e72b6c1a15b88d8c908c11066ece
kapilsarkar <k.sarkar81@gmail.com>
2023-07-24 12:08:08 +0530

    browserobjectmodel

---
+++ ADVANCE JAVASCRIPT-CONCEPTS/BOM-BROWSER OBJECT MODEL/script.js
@@ -0,0 +1,31 @@
+console.log(window.location)
+console.log(location.reload)
+
+// setTimeout(() => {
+//     location.reload()
+// }, 2000);
+
+console.log(location.href)
+
+//location.href='https://www.google.co.in/'
+
+console.log(window.history)
+
+console.log(history.back())
+
+console.log(history.forward())
+
+console.log(history.go(-1))
+
+console.log(history.go(+1))
+
+console.log(innerHeight)
+console.log(innerWidth)
+console.log(outerHeight)
+open('https://www.google.co.in/','google','resizable')
+//close('https://www.google.co.in/')
+
+resizeBy(600,500)
+
+moveTo(0,0)
+scrollBy(0,150)
