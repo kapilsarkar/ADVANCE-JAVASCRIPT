9328fe868facc4f4039cda4f18988ed9be712561
kapilsarkar <k.sarkar81@gmail.com>
2023-07-26 11:05:37 +0530

    innertextvstextcontent

---
+++ ADVANCE JAVASCRIPT-CONCEPTS/INNER TEXT VS TEXT CONTENT/script.js
@@ -0,0 +1,9 @@
+const paragraph = document.querySelector('p')
+
+console.log(paragraph.innerHTML)
+
+//paragraph.innerHTML ='<h4>Hi Kapil</h4>'
+
+console.log(paragraph.innerText)
+
+console.log(paragraph.textContent)
