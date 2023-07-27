ac4173abe004f963cf49dd04881bee0f39646270
kapilsarkar <k.sarkar81@gmail.com>
2023-07-27 06:51:51 +0530

    getattributeandsetattribute

---
+++ ADVANCE JAVASCRIPT-CONCEPTS/GET ATRRIBUTE AND SET ATTRIBUTE/script.js
@@ -0,0 +1,21 @@
+//const h1 = document.querySelector('h1')
+//const paragraph = document.querySelector('p')
+
+// console.log(paragraph.innerHTML);
+
+// paragraph.innerHTML = '<h4>Hiii</h4>'
+
+const atrributes = document.querySelector('[hello]').getAttribute('education')
+
+console.log(atrributes)
+
+const classAtrribute = document.querySelector('h1').getAttribute('class')
+
+console.log(classAtrribute)
+
+const setattr = document.querySelector('h1').setAttribute('title','JavaScript')
+
+console.log(setattr)
+
+const setid = document.querySelector('h1').setAttribute('id','heading')
+console.log(setid)
