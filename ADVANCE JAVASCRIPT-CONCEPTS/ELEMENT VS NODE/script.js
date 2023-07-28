58a6ae9fcd6e0d4d3fb57a0bc7975a521275afd8
kapilsarkar <k.sarkar81@gmail.com>
2023-07-28 07:09:44 +0530

    nodevselement

---
+++ ADVANCE JAVASCRIPT-CONCEPTS/ELEMENT VS NODE/script.js
@@ -0,0 +1,14 @@
+document.body.children
+
+document.body.childNodes[3]
+
+console.dir(document.body.childNodes[3])
+
+//console.log(Node)
+
+console.dir(document.body.attributes.style)
+console.dir(document)
+
+document.body.childNodes[2].nodeValue ="Namaste"
+
+//Every Element is a Node but every Node is not an element
