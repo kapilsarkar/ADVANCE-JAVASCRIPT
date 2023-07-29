fe47a6abb0eedd928e19fbadccc9855859714fa6
kapilsarkar <k.sarkar81@gmail.com>
2023-07-29 05:30:35 +0530

    initialcommit

---
+++ ADVANCE JAVASCRIPT-CONCEPTS/CREATE ELEMENT/script.js
@@ -0,0 +1,19 @@
+const h1 = document.querySelector("h1");
+const container = document.querySelector(".container");
+
+// container.appendChild(h1)
+// container.appendChild(h1.cloneNode(true))
+
+// for (let i = 2; i <=100; i++) {
+//     const newCard = card.cloneNode()
+//     newCard.innerText = i
+//     container.append(newCard)
+// }
+
+const newImg = document.querySelector("img");
+
+for (let i = 2; i <= 100; i++) {
+  const PokemonImg = newImg.cloneNode();
+  PokemonImg.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${i}.png`
+  container.append(PokemonImg)
+}
