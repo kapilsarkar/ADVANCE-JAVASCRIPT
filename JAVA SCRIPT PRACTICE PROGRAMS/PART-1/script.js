511dc518ab4148d45b2abf02cdea2c0a1f0d470c
kapilsarkar <k.sarkar81@gmail.com>
2023-08-10 06:42:24 +0530

    completed

--- JAVA SCRIPT PRACTICE PROGRAMS/PART-1/script.js
+++ JAVA SCRIPT PRACTICE PROGRAMS/PART-1/script.js
@@ -1,9 +1,36 @@
+//Q-1
 function fruit(){
-    console.log(fullName)
-    console.log(price)
+    console.log(fullName)  //Undefined
+    //console.log(price)  //Error
 
     var fullName = 'Kapil Sarkar'
     let price = 50
 }
 
 fruit()
+
+//Q-2
+
+for(var i =0; i< 3 ; i++){  //var has global scope
+    setTimeout(()=>
+        console.log(i),1
+    )
+}
+
+//Q-3
+
+for(let i =0 ; i< 3; i++){  // let has block scope
+    setTimeout(()=>{
+        console.log(i),1
+    })
+}
+
+//q-4
+
+console.log(+true)
+console.log(+typeof true)
+
+//Q-5
+
+console.log('Kapil')
+console.log(typeof ('Kapil'))
