d75c0e62904419e411fb888ea17c7015fc44852a
kapilsarkar <k.sarkar81@gmail.com>
2023-08-12 12:39:47 +0530

    somesectionsadded

--- JAVA SCRIPT PROJECTS/FOCUS ON TODAY/script.js
+++ JAVA SCRIPT PROJECTS/FOCUS ON TODAY/script.js
@@ -1,11 +1,21 @@
 const checkBoxList = document.querySelectorAll('.custom-checkbox')
 const inputFields = document.querySelectorAll('.goal-input')
+const errorLabel = document.querySelector('.error-label')
+const progressBar = document.querySelector('.progress-label')
 
 checkBoxList.forEach((checkbox)=>{
   checkbox.addEventListener('click',(e)=>{
+       const allGoalsAdded = [...inputFields].every(function(input){
+        return input.value
+       }) 
+       if(allGoalsAdded){
+        checkbox.parentElement.classList.toggle('completed')
+       }
+       else{
+         
+          progressBar.classList.add('show-error')
+       }
        
-       checkbox.parentElement.classList.toggle('completed')
   })
 })
 
-let allInputFields = false
