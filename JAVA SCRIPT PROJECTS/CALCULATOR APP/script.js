76807ca60ffa6be923378b5c6081113754487f57
kapilsarkar <k.sarkar81@gmail.com>
2023-08-15 12:57:10 +0530

    completed

--- JAVA SCRIPT PROJECTS/CALCULATOR APP/script.js
+++ JAVA SCRIPT PROJECTS/CALCULATOR APP/script.js
@@ -1,25 +1,28 @@
-let input = document.getElementById("input");
+(function() {
 
-let buttons = Array.from(document.getElementsByClassName("btn"));
+  let screen = document.querySelector('.screen');
+  let buttons = document.querySelectorAll('.btn');
+  let clear = document.querySelector('.btn-clear');
+  let equal = document.querySelector('.btn-equal');
 
-let operator = "";
-
-let data = [];
-
-buttons.map((button) => {
-  button.addEventListener("click", (e) => {
-    let buttonText = e.target.innerText;
-
-    if (buttonText === "AC") {
-      input.innerText = "";
-      data = [];
-      operator = "";
-    }
-    else if(buttonText == 'DE'){
-        input.innerText = input.innerText.slice(0,-1)
-    }
-    else if(buttonText === '.'){
-        input.innerText += buttonText
-    }
+  buttons.forEach(function(button) {
+      button.addEventListener('click', function(e) {
+              let value = e.target.dataset.num;
+              screen.value += value;
+          })
   });
-});
+
+  equal.addEventListener('click', function(e){
+      if(screen.value === ''){
+          screen.value = "";
+      }else{
+          let answer = eval(screen.value);
+          screen.value = answer;
+      }
+  })
+
+  clear.addEventListener('click', function(e){
+      screen.value = "";
+  })
+
+})();
