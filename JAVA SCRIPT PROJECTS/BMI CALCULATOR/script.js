72e487efebda9840dfded0ea4c266874330c5f5a
kapilsarkar <k.sarkar81@gmail.com>
2023-09-01 14:37:24 +0530

    projectadded

---
+++ JAVA SCRIPT PROJECTS/BMI CALCULATOR/script.js
@@ -0,0 +1,40 @@
+const form = document.querySelector("form");
+//this use case will give empty values
+// const height = parseInt(document.querySelector("#height").value);
+
+
+ 
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const height = parseInt(document.querySelector("#height").value);
+  const weight = parseInt(document.querySelector("#weight").value);
+  const results = document.querySelector("#results");
+  
+
+  if (height === '' || height <0 || isNaN(height)) {
+    results.innerHTML = `Please Give a Valid Height ${height}`
+  }
+
+  else if (weight === '' || weight <0 || isNaN(weight)) {
+    results.innerHTML = `Please Give a Valid Weight ${weight}`
+  }
+
+  else{
+   const bmi = (weight / ((height*height)/10000)).toFixed(2)
+   //show the result
+
+   if(bmi <18.6){
+    results.innerHTML = `<span>${bmi} Under Weight</span>`
+   }
+   else if(bmi >=18.6 && bmi <=24.9 ){
+        results.innerHTML =`<span>${bmi} Normal Range </span>`
+   }
+
+   else {
+    results.innerHTML = `<span>${bmi} OverWeight</span>`
+   }
+   //results.innerHTML = `<span>${bmi}</span>`
+  
+  }
+  
+});
