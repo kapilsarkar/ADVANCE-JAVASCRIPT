8951d3234b84405107e0e668c18ff4b7949cff90
kapilsarkar <k.sarkar81@gmail.com>
2023-09-09 12:50:20 +0530

    projectadded

--- JAVA SCRIPT PROJECTS/TO DO LIST/script.js
+++ JAVA SCRIPT PROJECTS/TO DO LIST/script.js
@@ -1,15 +1,58 @@
-let input = document.querySelector(".input");
+const input = document.querySelector(".input");
 const container = document.querySelector(".container");
 const btnAdd = document.querySelector(".btnAdd");
-
+const containerTask = document.querySelector(".containerTask");
 btnAdd.addEventListener("click", (e) => {
-  const inputValue = input.value;
-  let h2 = document.createElement("h2");
+  if (input.value == "") {
+    alert("Please Enter Some Task");
+  } else {
+    //Add Task
+    let inputValue = input.value;
+    let h2 = document.createElement("h2");
+    h2.innerHTML = inputValue;
+    h2.className = "TaskList";
+    containerTask.appendChild(h2);
 
-  //console.log(h2.innerText = inputValue)
-  h2.innerHTML = inputValue;
-  h2.style.backgroundColor = "green";
-  h2.style.color = "#fff";
-  container.appendChild(h2);
-  
+    //Delete Task
+    let Del = document.createElement("button");
+    Del.className = "DeleteBtn";
+    Del.innerText = "Delete";
+    h2.appendChild(Del);
+    input.value = "";
+    Del.addEventListener("click", (e) => {
+      h2.remove();
+    });
+
+    //Edit task
+    const Edit = document.createElement("button");
+    Edit.innerText = "Edit";
+    Edit.classList = "EditBtn";
+    h2.appendChild(Edit);
+
+    Edit.addEventListener("click", function (e) {
+      const UpdateBox = document.createElement("input");
+      UpdateBox.id = "Update";
+      Edit.remove();
+      const UpdateBtn = document.createElement("button");
+      UpdateBtn.innerText = "Update";
+      UpdateBtn.className = "UpdateBtn";
+      UpdateBtn.addEventListener("click", (e) => {
+        let updateValue = UpdateBox.value;
+        h2.innerText = updateValue;
+
+        Del = document.createElement("button");
+        Del.className = "DeleteBtn";
+        Del.innerText = "Delete";
+        h2.appendChild(Del);
+        h2.appendChild(Edit);
+        input.value = "";
+        Del.addEventListener("click", (e) => {
+          h2.remove();
+        });
+      });
+
+      h2.appendChild(UpdateBtn);
+      h2.appendChild(UpdateBox);
+    });
+  }
 });
