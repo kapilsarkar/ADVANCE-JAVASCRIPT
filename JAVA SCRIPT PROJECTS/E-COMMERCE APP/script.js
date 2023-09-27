973e7c5ae59066303202c4f02ea38966caaf991b
kapilsarkar <k.sarkar81@gmail.com>
2023-09-27 15:18:03 +0530

    somepartsadded

--- JAVA SCRIPT PROJECTS/E-COMMERCE APP/script.js
+++ JAVA SCRIPT PROJECTS/E-COMMERCE APP/script.js
@@ -2,14 +2,21 @@ const humBurgerMenu = document.querySelector(".humburger-menu");
 const closeIcon = document.querySelector(".close-icon");
 const navMenu = document.querySelector(".nav-menu");
 const menuContainer = document.querySelector(".menu-container");
+const darkMode = document.querySelector("#dark-mode");
 
 humBurgerMenu.addEventListener("click", () => {
   menuContainer.style.display = "block";
   humBurgerMenu.style.display = "none";
-   
 });
 closeIcon.addEventListener("click", () => {
   menuContainer.style.display = "none";
   humBurgerMenu.style.display = "block";
-  
 });
+
+darkMode.addEventListener("click", () => {
+  DarkLight();
+});
+
+function DarkLight() {
+  document.body.classList.toggle("dark");
+}
