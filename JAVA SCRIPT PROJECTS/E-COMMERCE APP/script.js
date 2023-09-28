be927a99834af3e24715c04add1378d23d7560d7
kapilsarkar <k.sarkar81@gmail.com>
2023-09-28 12:26:09 +0530

    somepartsadded

--- JAVA SCRIPT PROJECTS/E-COMMERCE APP/script.js
+++ JAVA SCRIPT PROJECTS/E-COMMERCE APP/script.js
@@ -11,6 +11,7 @@ humBurgerMenu.addEventListener("click", () => {
 closeIcon.addEventListener("click", () => {
   menuContainer.style.display = "none";
   humBurgerMenu.style.display = "block";
+  location.reload();
 });
 
 darkMode.addEventListener("click", () => {
@@ -20,3 +21,37 @@ darkMode.addEventListener("click", () => {
 function DarkLight() {
   document.body.classList.toggle("dark");
 }
+
+// coding for mobiles
+
+const mobile1Btn = document.querySelector(".mobile-1-btn");
+const cartNumber = document.querySelector(".cartNumber");
+const mobilePrice = document.querySelector(".mobile-1-price");
+const mobileText = document.querySelector(".mobile-1-text");
+const mobileImag = document.querySelector(".mobile-1-img");
+let num = 1;
+
+let fullCart = [
+  {
+    id: 1,
+    price: mobilePrice.innerText,
+    name: mobileText.innerText,
+    image: mobileImag.innerHTML,
+  },
+  // {
+  //   id: 2,
+  //   price: mobilePrice.innerText,
+  // },
+];
+
+
+
+mobile1Btn.addEventListener("click", (e) => {
+  fullCart.map((item) => {
+    console.log(item.price);
+    console.log(item.name);
+
+    cartNumber.innerText = num++;
+  });
+  mobile1Btn.disabled = true
+});
