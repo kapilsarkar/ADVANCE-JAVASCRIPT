8a3fd1c48df72715df807d14300411691d7b0ad1
kapilsarkar <k.sarkar81@gmail.com>
2023-10-03 11:42:12 +0530

    somepartsadded

--- JAVA SCRIPT PROJECTS/TASK MANAGER USING LOCAL STORAGE/script.js
+++ JAVA SCRIPT PROJECTS/TASK MANAGER USING LOCAL STORAGE/script.js
@@ -1,3 +1,10 @@
 const mainBoxInput = document.querySelector(".mainBox");
 const addTaskBtn = document.querySelector(".addTask");
 const displayOutput = document.querySelector('.display-container')
+
+//const myData = JSON.parse(localStorage.setItem)
+addTaskBtn.addEventListener('click',()=>{
+    let myData = [mainBoxInput.value];
+    console.log(myData)
+    localStorage.setItem('fullData',JSON.stringify(myData))
+})
