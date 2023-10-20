254e0f371b025d3f22fb6b434db498fc24a69dcf
kapilsarkar <k.sarkar81@gmail.com>
2023-10-20 07:37:51 +0530

    initialcommit

---
+++ ADVANCE JAVASCRIPT-CONCEPTS/SYNCHRONOUS VS ASYNCHRONOUS/script.js
@@ -0,0 +1,19 @@
+const button = document.querySelector("button");
+const image = document.querySelector("img");
+
+button.addEventListener("click", () => {
+    const xhr = new XMLHttpRequest();
+  
+    xhr.responseType = "json";
+    xhr.addEventListener("load", () => {
+      image.src = xhr.response.message;
+      console.log(xhr);
+    });
+    xhr.open("GET", "https://dog.ceo/api/breeds/image/random ");
+    xhr.send();
+  });
+  
+
+setTimeout(() => {
+  console.log("Hiii");
+}, 4000);
