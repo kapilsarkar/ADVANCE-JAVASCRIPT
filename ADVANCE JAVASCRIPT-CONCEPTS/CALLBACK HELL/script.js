ffde97d6c282617755be4c7d0966ad29add7817f
kapilsarkar <k.sarkar81@gmail.com>
2023-10-22 11:29:08 +0530

    initialcommit

---
+++ ADVANCE JAVASCRIPT-CONCEPTS/CALLBACK HELL/script.js
@@ -0,0 +1,27 @@
+console.log("Namste");
+
+setTimeout(function () {
+  console.log("JavaScript");
+}, 5000);
+
+console.log("Season2");
+
+// const cart = ["shoes", "pants", "kurta"];
+
+// api.createOrder(cart, function () {
+//   api.proceedToPayment(function () {
+//     api.showOrderSummary(function () {
+//       api.UpdateWallet();
+//     });
+//   });
+// });
+
+
+const xhr = new XMLHttpRequest()
+xhr.responseType = 'json'
+xhr.addEventListener('load', () => {
+  console.log(xhr.response.users[0]);
+})
+
+xhr.open('GET', 'https://dummyjson.com/users')
+xhr.send()
