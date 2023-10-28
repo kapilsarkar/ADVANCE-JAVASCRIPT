e5d0767493b365e64154e24c42fdba54198e2ef7
kapilsarkar <k.sarkar81@gmail.com>
2023-10-28 15:35:55 +0530

    callbackhell

--- ADVANCE JAVASCRIPT-CONCEPTS/CALLBACK HELL/script.js
+++ ADVANCE JAVASCRIPT-CONCEPTS/CALLBACK HELL/script.js
@@ -16,12 +16,24 @@ console.log("Season2");
 //   });
 // });
 
+function makeHttpRequest(method, url, callback) {
+  const xhr = new XMLHttpRequest();
+  xhr.responseType = "json";
+  xhr.addEventListener("load", () => {
+    console.log(xhr.response);
+    callback(xhr.response);
+  });
 
-const xhr = new XMLHttpRequest()
-xhr.responseType = 'json'
-xhr.addEventListener('load', () => {
-  console.log(xhr.response.users[0]);
+  xhr.open(method, url);
+  xhr.send();
+}
+
+makeHttpRequest('GET', 'https://dummyjson.com/users', (usersData) => {
+  makeHttpRequest('GET', `https://dummyjson.com/posts/user/${usersData.users[0].id}`, (postsData) => {
+    makeHttpRequest('GET', `https://dummyjson.com/comments/post/${postsData.posts[0].id}`, (commentsData) => {
+      makeHttpRequest('GET', `https://dummyjson.com/users/${commentsData.comments[0].user.id}`, (userData) => {
+        console.log(userData);
+      });
+    });
+  });
 })
-
-xhr.open('GET', 'https://dummyjson.com/users')
-xhr.send()
