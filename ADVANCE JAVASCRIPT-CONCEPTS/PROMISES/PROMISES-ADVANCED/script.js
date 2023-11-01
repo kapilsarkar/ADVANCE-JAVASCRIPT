27d1f94001cbad3968972f115c67ce14972e053e
kapilsarkar <k.sarkar81@gmail.com>
2023-11-01 16:20:08 +0530

    somepartsadded

--- ADVANCE JAVASCRIPT-CONCEPTS/PROMISES/PROMISES-ADVANCED/script.js
+++ ADVANCE JAVASCRIPT-CONCEPTS/PROMISES/PROMISES-ADVANCED/script.js
@@ -1,3 +1,9 @@
+const resolveBtn = document.querySelector("#resolveBtn");
 
+const p = new Promise((resolve, reject) => {
+  resolveBtn.addEventListener("click", () => {
+    resolve("Promise Resolved");
+  });
+});
 
-
+console.log(p);
