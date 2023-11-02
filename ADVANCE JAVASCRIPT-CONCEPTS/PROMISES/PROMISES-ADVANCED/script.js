00650636f68e863983f4b60088ee7026dd740272
kapilsarkar <k.sarkar81@gmail.com>
2023-11-02 06:54:07 +0530

    promises

--- ADVANCE JAVASCRIPT-CONCEPTS/PROMISES/PROMISES-ADVANCED/script.js
+++ ADVANCE JAVASCRIPT-CONCEPTS/PROMISES/PROMISES-ADVANCED/script.js
@@ -1,9 +1,24 @@
 const resolveBtn = document.querySelector("#resolveBtn");
-
+const rejectBtn = document.querySelector("#rejectBtn");
 const p = new Promise((resolve, reject) => {
   resolveBtn.addEventListener("click", () => {
     resolve("Promise Resolved");
   });
+  rejectBtn.addEventListener("click", () => {
+    reject("Promise Rejected");
+  });
 });
 
 console.log(p);
+
+p.then((data) => {
+  console.log("Hi Kapil Sarkar");
+  console.log(data);
+})
+  .catch((error) => {
+    console.log("Hi Piku Sarkar");
+    console.log(error);
+  })
+  .finally(() => {
+    console.log("Promises Settled");
+  });
