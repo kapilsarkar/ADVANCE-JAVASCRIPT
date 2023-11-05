b502d57e3f44da2974ee2bd87ed84010d6a3b75b
kapilsarkar <k.sarkar81@gmail.com>
2023-11-05 07:39:21 +0530

    somepartsadded

--- ADVANCE JAVASCRIPT-CONCEPTS/FETCH API/script.js
+++ ADVANCE JAVASCRIPT-CONCEPTS/FETCH API/script.js
@@ -1,5 +1,25 @@
-fetch("https://dummyjson.com/products").then((response) => {
-  response.json().then((data)=>{
-    console.log(data)
+fetch("https://dummyjson.com/products",{
+  method :"GET"
 })
+  .then((response) => response.json()
+  )
+  .then((data) => {
+    console.log(data);
+  }).catch((error)=>{
+    console.log(error)
+  })
+
+
+  fetch('https://dummyjson.com/products/add', {
+  method: 'POST',
+  headers: { 'Content-Type': 'application/json' },
+  body: JSON.stringify({
+    title: 'BMW Pencil',
+    category:'stationary',
+    /* other product data */
+  })
+})
+.then(res => res.json())
+.then(console.log).catch((error)=>{
+  console.log(error)
 })
