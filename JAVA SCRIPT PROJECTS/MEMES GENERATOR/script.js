c9a6d91109e0acc39fedd2e2be362f2beac3f167
kapilsarkar <k.sarkar81@gmail.com>
2023-11-17 15:31:35 +0530

    projectadded

--- JAVA SCRIPT PROJECTS/MEMES GENERATOR/script.js
+++ JAVA SCRIPT PROJECTS/MEMES GENERATOR/script.js
@@ -1,1 +1,27 @@
-const url = "https://meme-api.com/gimme/wholesomememes"
+//const url = "https://meme-api.com/gimme/wholesomememes"
+
+const generateBtn = document.querySelector(".generateBtn");
+const memeTitle = document.querySelector(".meme-title");
+const memeImage = document.querySelector(".meme-image");
+const authorOutput = document.querySelector(".authorName")
+
+function getMeme(){
+    fetch("https://meme-api.com/gimme/wholesomememes")
+    .then((response) => {
+     return response.json();
+    })
+    .then((data) => {
+        const {author,title,url} = data
+         console.log(data)
+         console.log(author)
+         console.log(title)
+         console.log(url)
+         memeTitle.innerText = title
+         authorOutput.innerText = author
+         memeImage.src = url
+    });
+}
+ getMeme()
+generateBtn.addEventListener("click", () => {
+   getMeme()
+});
