0653f8356371fc9e2d7bde6bf000e79eb0bf7e13
kapilsarkar <k.sarkar81@gmail.com>
2023-11-22 15:34:04 +0530

    somepartsadded

--- JAVA SCRIPT PROJECTS/DUMMY API PROJECT/script.js
+++ JAVA SCRIPT PROJECTS/DUMMY API PROJECT/script.js
@@ -4,16 +4,18 @@ const searchBoxInput = document.querySelector(".searchBox");
 const searchBtnInput = document.querySelector(".searchBtn");
 
 const displayContainerOutput = document.querySelector(".displayContainer");
-const url = "https://dummyjson.com/products/category/smartphones"
+const url = "https://official-joke-api.appspot.com/random_joke"
 searchBtnInput.addEventListener("click",(e)=>{
    fetch(url).then((response)=>{
     return response.json()
     
    }).then((data)=>{
     console.log(data)
+    const {punchline,setup,type} = data
+    console.log(punchline)
+    console.log(setup)
+    console.log(type)
      
-   }).then((products)=>{
-      console.log(products)
    }).catch((err)=>{
       console.log(err)
    })
