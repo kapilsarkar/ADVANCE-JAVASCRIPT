4153d40e5304e19bd97412eb39074977d3a9327e
kapilsarkar <k.sarkar81@gmail.com>
2023-11-28 19:08:08 +0530

    somepartsadded

--- JAVA SCRIPT PROJECTS/PRODUCT API PROJECT/script.js
+++ JAVA SCRIPT PROJECTS/PRODUCT API PROJECT/script.js
@@ -1,2 +1,23 @@
 //https://fakestoreapi.com/products
 
+const productsSelectInput = document.querySelector("#productsSelect");
+const searchBtnInput = document.querySelector(".SearchBtn");
+
+const displayContainerOutput = document.querySelector(".displayContainer");
+const url = "https://fakestoreapi.com/products"
+searchBtnInput.addEventListener("click",(e)=>{
+    async function productDisplay(){
+        try{
+            const apiData = await fetch(url)
+            const jsonValue = await apiData.json()
+            console.log(jsonValue)
+            
+        }
+        catch(err){
+            console.log(err)
+        }
+         
+    }
+    productDisplay()
+})
+
