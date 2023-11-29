50f85a5fc405aa0efd11b82e1ba306c5b0372df4
kapilsarkar <k.sarkar81@gmail.com>
2023-11-29 16:56:49 +0530

    somepartsadded

--- JAVA SCRIPT PROJECTS/PRODUCT API PROJECT/script.js
+++ JAVA SCRIPT PROJECTS/PRODUCT API PROJECT/script.js
@@ -1,23 +1,33 @@
 //https://fakestoreapi.com/products
 
-const productsSelectInput = document.querySelector("#productsSelect");
-const searchBtnInput = document.querySelector(".SearchBtn");
+// const productsSelectInput = document.querySelector("#productsSelect");
+// const searchBtnInput = document.querySelector(".SearchBtn");
 
-const displayContainerOutput = document.querySelector(".displayContainer");
-const url = "https://fakestoreapi.com/products"
-searchBtnInput.addEventListener("click",(e)=>{
-    async function productDisplay(){
-        try{
-            const apiData = await fetch(url)
-            const jsonValue = await apiData.json()
-            console.log(jsonValue)
-            
-        }
-        catch(err){
-            console.log(err)
-        }
+// const displayContainerOutput = document.querySelector(".displayContainer");
+// const url = "https://fakestoreapi.com/products"
+// searchBtnInput.addEventListener("click",(e)=>{
+//     async function productDisplay(){
+//         try{
+//             const apiData = await fetch(url)
+//             const jsonValue = await apiData.json()
+//             console.log(jsonValue)
+//            const category = jsonValue.filter((ct)=>{
+//                 if(ct.category === "men's clothing"){
+//                   return  ct
+//                 }
+                  
+//             })
+//             console.log(category)
+//             for(const eachCt in category ){
+//                 console.log(eachCt[0])
+//             }
+       
+//         }
+//         catch(err){
+//             console.log(err)
+//         }
          
-    }
-    productDisplay()
-})
+//     }
+//     productDisplay()
+// })
 
