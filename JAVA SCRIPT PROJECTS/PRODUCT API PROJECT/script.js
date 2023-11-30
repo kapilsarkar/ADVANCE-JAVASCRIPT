fa7f5c2178fb39d30ef592283a2c815311ffb38a
kapilsarkar <k.sarkar81@gmail.com>
2023-11-30 13:28:36 +0530

    somepartsadded

--- JAVA SCRIPT PROJECTS/PRODUCT API PROJECT/script.js
+++ JAVA SCRIPT PROJECTS/PRODUCT API PROJECT/script.js
@@ -1,33 +1,39 @@
 //https://fakestoreapi.com/products
-
-// const productsSelectInput = document.querySelector("#productsSelect");
-// const searchBtnInput = document.querySelector(".SearchBtn");
-
-// const displayContainerOutput = document.querySelector(".displayContainer");
-// const url = "https://fakestoreapi.com/products"
-// searchBtnInput.addEventListener("click",(e)=>{
-//     async function productDisplay(){
-//         try{
-//             const apiData = await fetch(url)
-//             const jsonValue = await apiData.json()
-//             console.log(jsonValue)
-//            const category = jsonValue.filter((ct)=>{
-//                 if(ct.category === "men's clothing"){
-//                   return  ct
-//                 }
-                  
-//             })
-//             console.log(category)
-//             for(const eachCt in category ){
-//                 console.log(eachCt[0])
-//             }
-       
-//         }
-//         catch(err){
-//             console.log(err)
-//         }
-         
-//     }
-//     productDisplay()
-// })
+const url = "https://fakestoreapi.com/products"
+const mensImg1 = document.querySelector(".men-part1Img")
+const mensDesc1 = document.querySelector(".men-part1Desc")
+const mensTitle1 = document.querySelector(".men-part1Title")
+const menPrice1 = document.querySelector(".men-Part1Price")
+async function mensDisplay(){
+    try{
+      const apiData = await fetch(url)
+      console.log(apiData)
+      const jsonValue = await apiData.json()
+      console.log(jsonValue)
+      const mensCategory = jsonValue.filter((ct)=>{
+        if(ct.category === "men's clothing"){
+            return ct
+        }
+      })
+      console.log(mensCategory)
+     const mensCategoryPart1 = mensCategory.filter((fr)=>{
+        if(fr.id === 1){
+            return fr
+        }
+     })
+     mensCategoryPart1.map((e)=>{
+        const men1id = e.id
+        mensDesc1.innerHTML = e.description
+        mensImg1.src = e.image
+        mensTitle1.innerText = e.title
+        menPrice1.innerText = `₹${e.price}`
+        
+     })
+     console.log(mensCategoryPart1)
+    }
+    catch(err){
+        console.log(err)
+    }
+}
+mensDisplay()
 
