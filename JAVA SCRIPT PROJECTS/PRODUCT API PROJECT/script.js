b45fa59fcc64fde3ac3c918246bacf249eacff1a
kapilsarkar <k.sarkar81@gmail.com>
2023-11-30 20:09:57 +0530

    somepartsadded

--- JAVA SCRIPT PROJECTS/PRODUCT API PROJECT/script.js
+++ JAVA SCRIPT PROJECTS/PRODUCT API PROJECT/script.js
@@ -4,6 +4,11 @@ const mensImg1 = document.querySelector(".men-part1Img")
 const mensDesc1 = document.querySelector(".men-part1Desc")
 const mensTitle1 = document.querySelector(".men-part1Title")
 const menPrice1 = document.querySelector(".men-Part1Price")
+
+const mensImg2 = document.querySelector(".men-part2Img")
+const mensDesc2 = document.querySelector(".men-part2Desc")
+const mensTitle2 = document.querySelector(".men-part2Title")
+const menPrice2 = document.querySelector(".men-Part2Price")
 async function mensDisplay(){
     try{
       const apiData = await fetch(url)
@@ -17,7 +22,7 @@ async function mensDisplay(){
       })
       console.log(mensCategory)
      const mensCategoryPart1 = mensCategory.filter((fr)=>{
-        if(fr.id === 1){
+        if(fr.id === 2){
             return fr
         }
      })
@@ -30,6 +35,19 @@ async function mensDisplay(){
         
      })
      console.log(mensCategoryPart1)
+
+     const mensCategoryPart2 = mensCategory.filter((fr)=>{
+        if(fr.id === 3){
+            return fr
+        }
+     })
+     mensCategoryPart2.map((e)=>{
+        const men2id = e.id
+        mensDesc2.innerHTML = e.description
+        mensImg2.src = e.image
+        mensTitle2.innerText = e.title
+        menPrice2.innerText = `₹${e.price}`
+     })
     }
     catch(err){
         console.log(err)
