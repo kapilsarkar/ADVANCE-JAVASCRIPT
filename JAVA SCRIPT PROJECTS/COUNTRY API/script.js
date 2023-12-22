3292261a7e4f66d8222c4c497a4d087bfd8d5895
kapilsarkar <k.sarkar81@gmail.com>
2023-12-22 16:33:49 +0530

    somepartsadded

--- JAVA SCRIPT PROJECTS/COUNTRY API/script.js
+++ JAVA SCRIPT PROJECTS/COUNTRY API/script.js
@@ -1,1 +1,17 @@
 // <!-- https://restcountries.com/v3.1/all -->
+
+// fetch('https://restcountries.com/v3.1/all').then((res)=>{
+//   return res.json()  
+// }).then((data)=>{
+//    data.forEach((country)=>{
+//       console.log(country)
+//    })
+// })
+
+
+const countryCard = document.createElement('a')
+countryCard.classList.add('countryCard')
+const cardImg = document.createElement("img")
+cardImg.src = "https://flagcdn.com/ss.svg"
+countryCard.append(cardImg)
+console.log(countryCard)
