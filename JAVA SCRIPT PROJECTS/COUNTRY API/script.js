a04a002b8553c0d1edb76469f09a29173573a018
kapilsarkar <k.sarkar81@gmail.com>
2023-12-23 16:06:15 +0530

    somepartsadded

--- JAVA SCRIPT PROJECTS/COUNTRY API/script.js
+++ JAVA SCRIPT PROJECTS/COUNTRY API/script.js
@@ -1,17 +1,25 @@
 // <!-- https://restcountries.com/v3.1/all -->
 
-// fetch('https://restcountries.com/v3.1/all').then((res)=>{
-//   return res.json()  
-// }).then((data)=>{
-//    data.forEach((country)=>{
-//       console.log(country)
-//    })
-// })
+const countriesContainer = document.querySelector(".countries-container");
 
+fetch("https://restcountries.com/v3.1/all")
+  .then((res) => {
+    return res.json();
+  })
+  .then((data) => {
+    data.forEach((country) => {
+      console.log(country);
+      const countryCard = document.createElement("a");
+      countryCard.classList.add("country-card");
+      countryCard.href =`/country.html?name=${country.name.common}`
+      countryCard.innerHTML = ` <img src=" ${country.flags.svg}" alt="flag" />
+    <div class="card-text">
+      <h3 class="card-title">${country.name.common}</h3>
+      <p><b>Population: </b>${country.population}</p>
+      <p><b>Region: </b>${country.region}</p>
+      <p><b>Capital: </b>${country.capital}</p>
+    </div>`;
 
-const countryCard = document.createElement('a')
-countryCard.classList.add('countryCard')
-const cardImg = document.createElement("img")
-cardImg.src = "https://flagcdn.com/ss.svg"
-countryCard.append(cardImg)
-console.log(countryCard)
+      countriesContainer.append(countryCard);
+    });
+  });
