438f613b42a6886f9c90a17ede4418a515b7b1e1
kapilsarkar <k.sarkar81@gmail.com>
2024-01-08 16:03:37 +0530

    somepartsadded

--- JAVA SCRIPT PROJECTS/COUNTRY API/script.js
+++ JAVA SCRIPT PROJECTS/COUNTRY API/script.js
@@ -1,43 +1,52 @@
 // <!-- https://restcountries.com/v3.1/all -->
 
-const countriesContainer = document.querySelector(".countries-container");
+const countriesContainer = document.querySelector('.countries-container')
+const filterByRegion = document.querySelector('.filter-by-region')
+const searchInput = document.querySelector('.search-container input')
+const themeChanger = document.querySelector('.theme-changer')
 
-//code for all countries
-fetch("https://restcountries.com/v3.1/all")
-  .then((res) => {
-    return res.json();
-  })
-  .then(renderCountries) 
+let allCountriesData
 
-  //code for filter by region
-
-  const filterByRegion = document.querySelector(".filter-by-region");
-
-  filterByRegion.addEventListener("change",(e)=>{
-    console.log(e.target.value)
-    fetch(`https://restcountries.com/v3.1/region/${filterByRegion.value}`)
-    .then((res) => {
-      return res.json();
-    })
-    .then(renderCountries) 
-    
+fetch('https://restcountries.com/v3.1/all')
+  .then((res) => res.json())
+  .then((data) => {
+    renderCountries(data)
+    allCountriesData = data
   })
 
-  function renderCountries(data){
-    countriesContainer.innerHTML = ''
-    data.forEach((country) => {
-      console.log(country);
-      const countryCard = document.createElement("a");
-      countryCard.classList.add("country-card");
-      countryCard.href =`/country.html?name=${country.name.common}`
-      countryCard.innerHTML = ` <img src=" ${country.flags.svg}" alt="${country.name.common}" />
-    <div class="card-text">
-      <h3 class="card-title">${country.name.common}</h3>
-      <p><b>Population: </b>${country.population}</p>
-      <p><b>Region: </b>${country.region}</p>
-      <p><b>Capital: </b>${country.capital}</p>
-    </div>`;
+filterByRegion.addEventListener('change', (e) => {
+  fetch(`https://restcountries.com/v3.1/region/${filterByRegion.value}`)
+    .then((res) => res.json())
+    .then(renderCountries)
+})
 
-      countriesContainer.append(countryCard);
-    });
-  }
+function renderCountries(data) {
+  countriesContainer.innerHTML = ''
+  data.forEach((country) => {
+    const countryCard = document.createElement('a')
+    countryCard.classList.add('country-card')
+    countryCard.href = `/country.html?name=${country.name.common}`
+    countryCard.innerHTML = `
+          <img src="${country.flags.svg}" alt="${country.name.common} flag" />
+          <div class="card-text">
+              <h3 class="card-title">${country.name.common}</h3>
+              <p><b>Population: </b>${country.population.toLocaleString(
+                'en-IN'
+              )}</p>
+              <p><b>Region: </b>${country.region}</p>
+              <p><b>Capital: </b>${country.capital?.[0]}</p>
+          </div>
+  `
+    countriesContainer.append(countryCard)
+  })
+}
+
+
+searchInput.addEventListener('input',  (e) => {
+  const filteredCountries = allCountriesData.filter((country) => country.name.common.toLowerCase().includes(e.target.value.toLowerCase()))
+  renderCountries(filteredCountries)
+})
+
+themeChanger.addEventListener('click', () => {
+  document.body.classList.toggle('dark')
+})
