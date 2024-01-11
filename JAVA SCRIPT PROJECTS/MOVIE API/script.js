62a2ab9f61182e4147f3f7d51e4fe7c1cf075bfa
kapilsarkar <k.sarkar81@gmail.com>
2024-01-11 17:07:43 +0530

    somepartsadded

--- JAVA SCRIPT PROJECTS/MOVIE API/script.js
+++ JAVA SCRIPT PROJECTS/MOVIE API/script.js
@@ -1,1 +1,11 @@
-//https://www.omdbapi.com/?i=tt3896198&apikey=2b65b40a
+//https://www.omdbapi.com/?i=tt3896198&apikey=2b65b40a\
+
+const BtnSearch = document.querySelector(".BtnSearch");
+let url = "https://www.omdbapi.com/?i=tt3896198&apikey=2b65b40a"
+BtnSearch.addEventListener("click",(e)=>{
+    fetch(url).then((res)=>{
+        return res.json()
+    }).then((data)=>{
+        console.log(data)
+    })
+})
