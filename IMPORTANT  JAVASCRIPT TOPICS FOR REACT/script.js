dd1b71870705b6dc91305378203dbe6f05e12f05
kapilsarkar <k.sarkar81@gmail.com>
2024-02-12 18:21:34 +0530

    updated

--- IMPORTANT  JAVASCRIPT TOPICS FOR REACT/script.js
+++ IMPORTANT  JAVASCRIPT TOPICS FOR REACT/script.js
@@ -269,5 +269,13 @@ console.log(booksAfterUpdate)
 
 //Promise and async await
 
+async function getTodos(){
+ const res = await fetch("https://jsonplaceholder.typicode.com/todos")
+ const data = await res.json()
+ console.log(data)
+ return data
+}
 
+const todos = getTodos()
+console.log(todos)
 
