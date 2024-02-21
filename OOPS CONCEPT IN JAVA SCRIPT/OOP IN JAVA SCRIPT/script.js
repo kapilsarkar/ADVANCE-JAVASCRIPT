584c44960a4e462a27af93785e1ae2e30973741d
kapilsarkar <k.sarkar81@gmail.com>
2024-02-21 18:56:32 +0530

    somepartsadded

--- OOPS CONCEPT IN JAVA SCRIPT/OOP IN JAVA SCRIPT/script.js
+++ OOPS CONCEPT IN JAVA SCRIPT/OOP IN JAVA SCRIPT/script.js
@@ -1,16 +1,10 @@
-describe('functionLength', () => {
-    test('no arguments', () => {
-      expect(functionLength(function foo() {})).toBe(0);
-      expect(functionLength(() => {})).toBe(0);
-    });
-  
-    test('one argument', () => {
-      expect(functionLength(function foo(a) {})).toBe(1);
-      expect(functionLength((a) => {})).toBe(1);
-    });
-  
-    test('two arguments', () => {
-      expect(functionLength(function foo(a, b) {})).toBe(2);
-      expect(functionLength((a, b) => {})).toBe(2);
-    });
-  });
+const user = {
+  fitstName :"Kapil",
+  lastName: "Sarkar",
+  age:32,
+
+  getAgeYear: function(){
+    return console.log(new Date().getFullYear() - user.age) 
+  }
+}
+console.log(user.getAgeYear())
