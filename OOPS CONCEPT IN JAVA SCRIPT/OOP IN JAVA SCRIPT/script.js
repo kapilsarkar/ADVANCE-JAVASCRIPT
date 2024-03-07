392c7f8000d4a8b4f99f6fddff762c0171044939
kapilsarkar <k.sarkar81@gmail.com>
2024-03-07 12:26:06 +0530

    somepartsadded

--- OOPS CONCEPT IN JAVA SCRIPT/OOP IN JAVA SCRIPT/script.js
+++ OOPS CONCEPT IN JAVA SCRIPT/OOP IN JAVA SCRIPT/script.js
@@ -1,11 +1,35 @@
-const user = {
-  fitstName :"Kapil",
-  lastName: "Sarkar",
-  age:32,
+// const user1 = {
+//   firstName :"Kapil",
+//   lastName: "Sarkar",
+//   age:32,
 
-  getAgeYear: function(){
-    return console.log(new Date().getFullYear() - user.age) 
+//   getAgeYear: function(){
+//     return console.log(new Date().getFullYear() - user1.age) 
+//   }
+// }
+// console.log(user1.getAgeYear())
+
+function createUser(firstName,lastName,age){
+  const user = {
+    firstName : firstName,
+    lastName: lastName,
+    age:age,
+
+    getAgeYear: function(){
+      return new Date().getFullYear() - user.age
+    }
+   
   }
+  return user
 }
-console.log(user.getAgeYear())
+ 
 
+const user1 = createUser("John","Cena",46)
+
+console.log(user1)
+console.log(user1.getAgeYear())
+
+const user2 = createUser("Kapil","Sarkar",33)
+
+console.log(user2)
+console.log(user2.getAgeYear())
