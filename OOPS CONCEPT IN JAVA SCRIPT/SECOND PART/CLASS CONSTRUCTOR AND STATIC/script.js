ebc656aa77e35c7f577729764649660c722c503d
kapilsarkar <k.sarkar81@gmail.com>
2024-03-17 07:24:08 +0530

    partsadded

---
+++ OOPS CONCEPT IN JAVA SCRIPT/SECOND PART/CLASS CONSTRUCTOR AND STATIC/script.js
@@ -0,0 +1,11 @@
+class User {
+  constructor(username,email,password){
+    this.username = username
+    this.email = email
+    this.password = password
+  }
+
+}
+
+//A constructor is called whenever qn object is initialized from class a constructor is called
+
