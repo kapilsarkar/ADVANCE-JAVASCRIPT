57c6a87e8b14a81b75a1546cf371991dbed8dc91
kapilsarkar <k.sarkar81@gmail.com>
2024-03-24 13:11:30 +0530

    added

--- OOPS CONCEPT IN JAVA SCRIPT/SECOND PART/THIS KEYWORD/script.js
+++ OOPS CONCEPT IN JAVA SCRIPT/SECOND PART/THIS KEYWORD/script.js
@@ -1,13 +1,4 @@
 "use strict"
-//this inside strict mode --- (this substitution)
-
-//this value depends on how this is called (window)
-
-//this inside object's method
-
-//call apply and bind methods (sharing methods)
-
-//this inside arrow function
 
 //this inside nested arrow function
 
@@ -27,6 +18,57 @@ function x(){
 x()  //--- this keyword works differently in strict mode and non-strict mode --> here it is in non-strict mode
 
 
+//this inside non-strict mode --- (this substitution)
+
+//according to this this substitution--- if the value of this keyword is undefined or null this will will be replaced  with global object --- only in non strict mode
+
+//what is the value of this keyword inside a function ?
+
+//the value of this keyword inside a function is undefined but because java script has something known as this substitution so the value becomes equal to global object if we will not use strict mode
+ 
+//this keyword value depends on how this is called (window)---
+x()
+window.x() 
+// if the function is called without any reference of any object--(x)-- then the value is undefined
+// if the function is called with reference then it will give window object --window.x()--
 
 
+//this inside object's method
+// difference between function and method in java script--- if we will make a function as a part of an object then it is known as method
+const obj = {
+ a: 10,
+ x: function(){  // here x is a method because the function x is part of object
+   console.log(this.a)
+ }
+}
 
+obj.x()
+
+//call apply and bind methods (sharing methods)
+
+const student = {
+  name : "Kapil",
+  printName : function(){
+    console.log(this.name)
+  }
+}
+student.printName()
+
+//Now sharing of method printName from student to student2
+const student2 = {
+  name: "Piku",
+
+}
+//value of this can be modified by call,apply and bind method
+student.printName.call(student2)  //now value of this is student2
+
+//this keyword inside arrow function
+// arrow function do not have their this they take the value of their lexical environment where they are enclosed.
+const obj2 = {
+  a: 10,
+  x:()=>{
+    console.log(this)
+  }
+}
+
+obj2.x()
