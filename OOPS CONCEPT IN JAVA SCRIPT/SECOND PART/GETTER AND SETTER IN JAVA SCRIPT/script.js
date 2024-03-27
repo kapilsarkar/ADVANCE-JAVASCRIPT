b64d66fc79c0b28b98c40b07a2ac39f03a3ef351
kapilsarkar <k.sarkar81@gmail.com>
2024-03-27 08:06:20 +0530

    somepartsadded

--- OOPS CONCEPT IN JAVA SCRIPT/SECOND PART/GETTER AND SETTER IN JAVA SCRIPT/script.js
+++ OOPS CONCEPT IN JAVA SCRIPT/SECOND PART/GETTER AND SETTER IN JAVA SCRIPT/script.js
@@ -3,8 +3,21 @@ class User {
     this.email = email;
     this.password = password;
   }
+  get email(){
+    return this._email.toUpperCase()
+  }
+  set email(value){
+    this._email = value
+  }
+  get password(){
+    return `${this._password}kapil`
+  }
+  set password(value){
+      this._password = value.toUpperCase()
+  }
 }
 
-const kapil = new User("kapil@gmail.com","123")
+const kapil = new User("kapil@gmail.com","abc")
 
 console.log(kapil.password)
+console.log(kapil.email)
