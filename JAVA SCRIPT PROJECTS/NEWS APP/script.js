ed3b1d373113c61a2171086e7838a2e523c54a5a
kapilsarkar <k.sarkar81@gmail.com>
2024-04-01 17:46:36 +0530

    somepartsadded

--- JAVA SCRIPT PROJECTS/NEWS APP/script.js
+++ JAVA SCRIPT PROJECTS/NEWS APP/script.js
@@ -1,18 +1,29 @@
-let url = "https://newsapi.org/v2/everything?q=viratkohli&apiKey=9a6cad2c29524869be31815d8d710b4d"
-let key = "9a6cad2c29524869be31815d8d710b4d"
+// let url = "https://newsapi.org/v2/everything?q=viratkohli&apiKey=9a6cad2c29524869be31815d8d710b4d"
+// let key = "9a6cad2c29524869be31815d8d710b4d"
 
-async function news(){
-    let newsData = await fetch(url)
-    let jsonData = await newsData.json()
-    console.log(jsonData)
-    //console.log(jsonData.articles)
+// async function news(){
+//     let newsData = await fetch(url)
+//     let jsonData = await newsData.json()
+//     console.log(jsonData)
+//     //console.log(jsonData.articles)
 
   
-}
+// }
 
-news()
+// news()
 
 
 //9a6cad2c29524869be31815d8d710b4d
 
 //https://newsapi.org/v2/everything?q=virat&apiKey=9a6cad2c29524869be31815d8d710b4d
+
+let url = "https://newsapi.org/v2/everything?q=virat&kohli&apiKey=9a6cad2c29524869be31815d8d710b4d"
+
+async function news(){
+    let newsData = await fetch(url)
+    let jsonValue = await newsData.json()
+    console.log(newsData)
+    console.log(jsonValue)
+}
+
+news()
