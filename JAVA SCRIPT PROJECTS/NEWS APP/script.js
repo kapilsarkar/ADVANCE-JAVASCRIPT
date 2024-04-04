b4662a4b5efe42b28be25b649e8f475ee0175c3e
kapilsarkar <k.sarkar81@gmail.com>
2024-04-04 17:20:51 +0530

    somepartsadded

--- JAVA SCRIPT PROJECTS/NEWS APP/script.js
+++ JAVA SCRIPT PROJECTS/NEWS APP/script.js
@@ -7,23 +7,40 @@
 //     console.log(jsonData)
 //     //console.log(jsonData.articles)
 
-  
 // }
 
 // news()
 
-
 //9a6cad2c29524869be31815d8d710b4d
 
 //https://newsapi.org/v2/everything?q=virat&apiKey=9a6cad2c29524869be31815d8d710b4d
+let key = "9a6cad2c29524869be31815d8d710b4d";
+let url = `https://newsapi.org/v2/everything?q=politics&apiKey=${key}`;
+let newsSection = document.querySelector(".news-section");
 
-let url = "https://newsapi.org/v2/everything?q=india&apiKey=9a6cad2c29524869be31815d8d710b4d"
+async function news() {
+  let newsData = await fetch(url);
+  let jsonValue = await newsData.json();
+  console.log(newsData);
+  console.log(jsonValue);
 
-async function news(){
-    let newsData = await fetch(url)
-    let jsonValue = await newsData.json()
-    console.log(newsData)
-    console.log(jsonValue)
+  jsonValue.articles.map((e) => {
+    console.log(e);
+
+    let newsCardDiv = document.createElement("div");
+    newsCardDiv.classList.add("news-card");
+    let newsTitle = document.createElement("h3");
+    newsTitle.classList.add("news-card-title");
+    let newsImage = document.createElement("img");
+    newsImage.classList.add("news-card-img");
+    let newsDesc = document.createElement("p");
+    newsDesc.classList.add("news-desc");
+    newsTitle.innerText = e.title
+    newsImage.src=e.urlToImage
+    newsDesc.innerText = e.description
+    newsCardDiv.append(newsTitle,newsImage,newsDesc)
+    newsSection.appendChild(newsCardDiv)
+  });
 }
 
-news()
+news();
